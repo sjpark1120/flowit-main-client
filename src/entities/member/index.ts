bb9afce931dd10098ab441ae66416927c734755b
sjpark1120 <asgol9609@gmail.com>
2026-06-14 22:38:49 +0900

    Feat: 초대 코드로 워크스페이스 가입 API 폴더 이동

--- src/entities/member/index.ts
+++ src/entities/member/index.ts
@@ -1,1 +1,5 @@
-export { joinWorkspaceByInviteCode } from './join-workspace-by-invite-code';
+export { joinWorkspaceByInviteCode } from './api/join-workspace-by-invite-code';
+export type {
+    JoinWorkspaceByInviteCodeRequest,
+    JoinWorkspaceByInviteCodeResponse,
+} from './model/join-workspace-by-invite-code.types';
